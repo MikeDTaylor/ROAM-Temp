45c624d699f01c118acaf3b456cc888eee451821
Mike.Taylor <michael.d.t.taylor@gmail.com>
2023-03-07 10:37:06 +0800

    Test script for testing github collaboration

---
+++ TestScript.R
@@ -0,0 +1,14 @@
+# Clone repo and set up new project
+
+# usethis::create_from_github(
+  # "https://github.com/MikeDTaylor/ROAM-Temp.git",
+  # destdir = "C:/Users/21656888/GIT/"
+# )
+
+# Random code ----
+
+for (i in 1:10){
+
+  print(i)
+
+}
